4d67ff51fad3c13b06785aeaca27592165945934
rakabgs27 <rakabgs2000@gmail.com>
2022-02-23 14:54:07 +0700

    Tugas Minggu ke - 2 Prak 3

--- routes/web.php
+++ routes/web.php
@@ -43,3 +43,51 @@ Route::get('/', [HomeController::class, 'index']);
 Route::get('/about', [AboutController::class, 'about']); 
 
 Route::get('/articles/{id}', [ArticleController::class, 'articles']); 
+
+//Praktikum 3 
+//Halaman Home
+Route::get('/home', function () {
+    echo "Hi! Selamat Datang di Halaman HOME";
+});
+
+Route::prefix('category') -> group(function(){
+    Route::get("/marbel-edu-games", function(){
+        echo "Tampilan marbel edu games";
+    });
+    Route::get("/marbel-and-friends-kids-games", function(){
+        echo "Tampilan marbel and friends kids games";
+    });
+    Route::get("/riri-story-books", function(){
+        echo "Tampilan riri story books";
+    });
+    Route::get("/kolak-kids-songs", function(){
+        echo "Tampilan kolak kids songs";
+    });
+});
+
+Route::prefix('news') -> group(function(){
+    Route::get("", function(){
+        echo "Tampilan news";
+    });
+    Route::get("/educa-studio-berbagi-untuk-warga-sekitar-terdampak-covid-19", function(){
+        echo "Tampilan educa studio berbagi untuk warga sekitar terdampak covid 19";
+    });
+});
+
+Route::prefix('program') -> group(function(){
+    Route::get("/karir" , function(){
+        echo "Tampilan karir";
+    });
+    Route::get("/magang" , function(){
+        echo "Tampilan magang";
+    });
+    Route::get("/kunjungan-industri" , function(){
+        echo "Tampilan kunjungan industri";
+    });
+});
+
+Route::get("/about-us" , function(){
+    echo "ABOUT US <br><br> Raka Bagas Fitriansyah <br> 2041720187 <br> TI - 2G";
+});
+
+
